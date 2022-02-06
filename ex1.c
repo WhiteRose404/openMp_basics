b33525a8bd7ad1042d6dec2f2d3f7a5508f0bb02
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-06 21:42:29 +0100

    getting rid of false sharing and using atomic for software acceleration

--- ex1.c
+++ ex1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <omp.h>
 /*
 	calculting the value of PI using the following appr intergal(4/(1+x^2)) = PI
@@ -6,23 +7,25 @@
 */
 
 
-int main(){
-	int num_steps = 10000;
-	double step = 1.0 /( (double) num_steps),sum = 0;
-	#pragma omp parallel num_threads(5)
+int main(int argc,char* argv[]){
+	int num_steps = 100000000,gsize;
+	double step = 1.0 /((double)num_steps),sum = 0, start = omp_get_wtime();
+	#pragma omp parallel num_threads(20)
 	{
 		int id = omp_get_thread_num();
 		int size = omp_get_num_threads();
+		if(!id)	gsize = size;
 		double psum=0;
 		for(int i=id;i<num_steps;i+=size){
 			double x = (0.5+i)*step;
 			double f = 4.0 /( (double) 1+x*x);
 			psum+=f;
 		}
-		printf("thread %d: has %lf\n",id,psum*step);
-		sum+=psum;
+		#pragma omp atomic
+			sum+=psum;
 	}
+	double time = omp_get_wtime()-start;
 	sum = sum*step;
-	printf("PI=%lf\n",sum);
+	printf("PI=%.13lf took around %lf using %d threads\n",sum,time,gsize);
 	return 0;
 }
