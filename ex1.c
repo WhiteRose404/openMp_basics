b3961efc0344b3cf1ef584d7a8068d414adab966
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-06 17:33:38 +0100

    build it but the algo is nowhere stable race condition is lurking around

--- ex1.c
+++ ex1.c
@@ -7,12 +7,20 @@
 
 
 int main(){
-	int num_steps = 100;
+	int num_steps = 10000;
 	double step = 1.0 /( (double) num_steps),sum = 0;
-	for(int i=0;i<num_steps;i++){
-		double x = (0.5+i)*step;
-		double f = 4.0 /( (double) 1+x*x);
-		sum+=f;
+	#pragma omp parallel num_threads(5)
+	{
+		int id = omp_get_thread_num();
+		int size = omp_get_num_threads();
+		double psum=0;
+		for(int i=id;i<num_steps;i+=size){
+			double x = (0.5+i)*step;
+			double f = 4.0 /( (double) 1+x*x);
+			psum+=f;
+		}
+		printf("thread %d: has %lf\n",id,psum*step);
+		sum+=psum;
 	}
 	sum = sum*step;
 	printf("PI=%lf\n",sum);
